09bcb6a185168eef0c277183ec4978ce38a0814a
Marina Zinchenko <amanda79116@gmail.com>
2022-12-11 15:07:36 -0600

    JUnit4 and JUnit Jupiter added

--- src/test/java/ru/netology/unit/CashbackHackServiceJupiterTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceJupiterTest.java
@@ -1,39 +1,40 @@
 package ru.netology.unit;
 
-public class CashbackHackServiceTest {
-    @org.junit.Test
+
+public class CashbackHackServiceJupiterTest {
+    @org.junit.jupiter.api.Test
     public void ShouldReturn1000WhenPassed0() {
         int amount = 0;
         int expected = 1000;
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
-        org.junit.Assert.assertEquals(expected, actual);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 
-    @org.junit.Test
+    @org.junit.jupiter.api.Test
     public void ShouldReturn200WhenPassed800() {
         int amount = 800;
         int expected = 200;
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
-        org.junit.Assert.assertEquals(expected, actual);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 
-    @org.junit.Test
+    @org.junit.jupiter.api.Test
     public void ShouldReturn500WhenPassed1500() {
         int amount = 1500;
         int expected = 500;
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
-        org.junit.Assert.assertEquals(expected, actual);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 
-    @org.junit.Test
+    @org.junit.jupiter.api.Test
     public void ShouldReturn0WhenPassed1000() {
         int amount = 1000;
         int expected = 0;
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
-        org.junit.Assert.assertEquals(expected, actual);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 }
